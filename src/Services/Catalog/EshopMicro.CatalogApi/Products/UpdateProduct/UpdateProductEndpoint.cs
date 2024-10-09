161db13bd0cef8d19243143bda7563ee9539a932
Arturo J Lira Perez <ajlirap@gmail.com>
2024-10-08 22:29:57 -0700

    Implemented the UpdateProductEndpoint

--- src/Services/Catalog/EshopMicro.CatalogApi/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Services/Catalog/EshopMicro.CatalogApi/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -1,6 +1,33 @@
-﻿namespace EshopMicro.CatalogApi.Products.UpdateProduct;
+﻿
+namespace EshopMicro.CatalogApi.Products.UpdateProduct;
 
+public record UpdateProductRequest(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
 
-public class UpdateProductEndpoint
+public record UpdateProductResponse (bool IsSuccess);
+
+public class UpdateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products",
+            async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName(nameof(UpdateProductResponse))
+            .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Update a product")
+            .WithDescription("Update a product by its id");
+    }
 }
