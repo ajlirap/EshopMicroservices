5bcfaea6daaa32b1b5509841cc059db6b4b91d80
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-16 04:58:49 -0800

    Implemented the Crud operations for the Basket API

--- src/Basket/Basket.API/Program.cs
+++ src/Basket/Basket.API/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //1 . Add services to the container.
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+}); 
+
 
 var app = builder.Build();
 
 //2. Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
