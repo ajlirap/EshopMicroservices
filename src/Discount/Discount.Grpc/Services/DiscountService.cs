f5c2cb5f9050f6f185b882205105b2322d213b8b
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-17 21:43:21 -0800

    Created Coupon GRPC

--- src/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -32,10 +32,23 @@ public class DiscountService
         var couponModel = coupon.Adapt<CouponModel>();
         return couponModel;
     }
-    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
     {
-        return base.CreateDiscount(request, context);
+        var coupon = request.Coupon.Adapt<Coupon>();
 
+        if (coupon == null)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Discount Request"));
+        }
+
+        dbContext.Coupons.Add(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is successfully created. ProductName : {ProductName}", coupon.ProductName);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+
+        return couponModel;
     }
     public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
     {
