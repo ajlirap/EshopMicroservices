3b7d198574abd4672de922a22f1a095822b8d449
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-22 00:48:09 -0800

    Implementing in Application Layer GetOrderQuery and Hanler

---
+++ src/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrderQuery.cs
@@ -0,0 +1,8 @@
+﻿using EshopMicro.Common.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrderQuery(PaginationRequest PaginationRequest) :
+    IQuery<GetOrdersResult>;
+
+public record GetOrdersResult(PaginationResult<OrderDto> Orders);
---
+++ src/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,32 @@
+﻿using EshopMicro.Common.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public class GetOrdersHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetOrderQuery, GetOrdersResult>
+    {
+        public async Task<GetOrdersResult> Handle(GetOrderQuery query, CancellationToken cancellationToken)
+        {
+            //get orders with pagination
+
+            var pageIndex = query.PaginationRequest.PageIndex;
+            var pageSize = query.PaginationRequest.PageSize;
+
+            var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+            var orders = await dbContext.Orders
+                .Include(x => x.OrderItems)
+                .OrderBy(o=> o.OrderName.Value)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersResult(
+                new PaginationResult<OrderDto>(
+                    pageIndex,
+                    pageSize,
+                    totalCount,
+                    orders.ToOrderDtoList()));
+        }
+    }
+}
