824a97da7e46fc1849f4822cee915b3be2fb91b0
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-21 16:03:15 -0800

    Implementing CQRS in Application Layer

---
+++ src/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,24 @@
+﻿using EshopMicro.Common.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+public record CreateOrderCommand (OrderDto order)
+    : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderValidator()
+    {
+        RuleFor(x => x.order.OrderName)
+            .NotEmpty().WithMessage("Name is required");
+
+        RuleFor(x=> x.order.CustomerId)
+            .NotNull().WithMessage("CustomerId is required");
+
+        RuleFor(x => x.order.OrderItems)
+            .NotEmpty().WithMessage("OrderItems Should not be empty");
+    }
+}
