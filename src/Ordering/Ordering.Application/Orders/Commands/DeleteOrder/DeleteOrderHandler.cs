d3b37dd051d46a0d6ab4dbc58aad31ef1a3d8c60
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-21 22:35:33 -0800

    Implementing Delete feature in Application Layer

---
+++ src/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler (IApplicationDbContext dbContext)
+    :ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        //Delete Order Entity from command object
+
+        var orderId = OrderId.Of(request.OrderId);
+        var order = await dbContext.Orders.FindAsync([orderId], cancellationToken);
+
+        if (order is null)
+        {
+            throw new OrderNotFoundException(request.OrderId);
+        }
+
+        //Save to database
+        dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        //Return result
+        return new DeleteOrderResult(true); 
+    }
+}
