6c45c992775e2cf10b67915cfd90cb296909d622
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-22 12:22:29 -0800

    Create Endpoints and Implementing Create Order

--- src/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,42 +1,19 @@
-﻿namespace Ordering.API;
+﻿using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
-        //services.AddAutoMapper(typeof(MappingProfile));
-        //services.AddMediatR(typeof(CreateOrderCommand).GetTypeInfo().Assembly);
-        //services.AddValidatorsFromAssembly(typeof(CreateOrderCommand).GetTypeInfo().Assembly);
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehaviour<,>));
-        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheBehaviour<,>));
+        services.AddCarter();
 
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
-        //app.MapCarter();
+        app.MapCarter();
 
         return app;
     }
---
+++ src/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Ordering.Application.Dtos;
+global using Carter;
+global using Mapster;
+global using MediatR;
+
