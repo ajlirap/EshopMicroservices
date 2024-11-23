a2a0889418f17699862d3aa0b2e0738e686615d5
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-22 17:48:39 -0800

    Implementation of GetOrdersByName in Order API

--- src/Ordering/Ordering.API/EndPoints/GetOrdersByName.cs
+++ src/Ordering/Ordering.API/EndPoints/GetOrdersByName.cs
@@ -1,6 +1,26 @@
-﻿namespace Ordering.API.EndPoints
+﻿using Ordering.Application.Orders.Queries.GetOrdersByName;
+
+namespace Ordering.API.EndPoints;
+
+public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByName : ICarterModule
 {
-    public class GetOrdersByName
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+
+            var response = result.Adapt<GetOrdersByNameResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByName")
+        .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Gets orders by name")
+        .WithDescription("Gets orders by name in the system.");
     }
 }
