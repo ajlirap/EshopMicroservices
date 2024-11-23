cb363668e2f73ebc88b3b7e3e4b659626f2028f5
Arturo J Lira Perez <ajlirap@gmail.com>
2024-11-22 17:20:54 -0800

    Implementing UpdateOrder EndPoint in Order API

--- src/Ordering/Ordering.API/EndPoints/UpdateOrder.cs
+++ src/Ordering/Ordering.API/EndPoints/UpdateOrder.cs
@@ -1,6 +1,28 @@
-﻿namespace Ordering.API.EndPoints
+﻿
+using Ordering.Application.Orders.Commands.UpdateOrder;
+
+namespace Ordering.API.EndPoints;
+
+public record UpdateOrderRequest(Guid Id, OrderDto order);
+
+public record UpdateOrderResponse(bool IsSuccess);
+public class UpdateOrder : ICarterModule
 {
-    public class UpdateOrder
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateOrderCommand>();
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateOrder")
+        .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Updates an existing order")
+        .WithDescription("Updates an existing order in the system.");
     }
 }
