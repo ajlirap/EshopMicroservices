c490cbeefec7e73f25f9d0ac57e44c8390acb765
Arturo J Lira Perez <ajlirap@gmail.com>
2024-12-02 13:27:00 -0800

    Implementing Cart operation and integrating basket service

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,6 +1,6 @@
 ﻿global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Basket;
+global using Shopping.Web.Services;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.RazorPages;
-global using Shopping.Web.Services;
 global using Refit;
-
