2e0c7ac839035b56815b181eccaedd8a7a5e91c6
Arturo J Lira Perez <ajlirap@gmail.com>
2024-12-02 19:09:02 -0800

    Implementing Product details.

--- src/WebApps/Shopping.Web/Pages/ProductDetail.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/ProductDetail.cshtml.cs
@@ -1,22 +1,26 @@
 namespace Shopping.Web.Pages
 {
-    public class IndexModel (ICatalogService catalogService,
-        IBasketService basketService,
-        ILogger<IndexModel> logger)
+    public class ProductDetailModel
+        (ICatalogService catalogService, IBasketService basketService, ILogger<ProductDetailModel> logger)
         : PageModel
     {
-        public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
+        public ProductModel Product { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync()
+        [BindProperty]
+        public string Color { get; set; } = default!;
+
+        [BindProperty]
+        public int Quantity { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid productId)
         {
-            logger.LogInformation("Index page visited");
-            var response = await catalogService.GetProducts();
-            ProductList = response.Products;
+            var response = await catalogService.GetProduct(productId);
+            Product = response.Product;
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCardAsync(Guid productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
         {
             logger.LogInformation("Add to cart button clicked");
             var productResponse = await catalogService.GetProduct(productId);
@@ -28,8 +32,8 @@ namespace Shopping.Web.Pages
                 ProductId = productId,
                 ProductName = productResponse.Product.Name,
                 Price = productResponse.Product.Price,
-                Quantity = 1,
-                Color = "Black"
+                Quantity = Quantity,
+                Color = Color
             });
 
             await basketService.StoreBasket(new StoreBasketRequest(basket));
